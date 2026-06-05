8f2481a0581e4c414faebdbe3b445ce298e4824d
hyk <1770848824@qq.com>
2026-06-05 06:18:50 +0000

    1

--- lib/rack.rb
+++ lib/rack.rb
@@ -8,57 +8,16 @@
 # The Rack main module, serving as a namespace for all core Rack
 # modules and classes.
 #
-# All modules meant for use in your application are <tt>autoload</tt>ed here,
-# so it should be enough just to <tt>require 'rack'</tt> in your code.
-
+# All modules meant for use in your application are loaded via
+# <tt>rack/autoloads</tt>, so it should be enough just to
+# <tt>require 'rack'</tt> in your code.
 require_relative 'rack/version'
 require_relative 'rack/constants'
+require_relative 'rack/autoloads'
 
 module Rack
-  autoload :BadRequest, "rack/bad_request"
-  autoload :BodyProxy, "rack/body_proxy"
-  autoload :Builder, "rack/builder"
-  autoload :Cascade, "rack/cascade"
-  autoload :CommonLogger, "rack/common_logger"
-  autoload :ConditionalGet, "rack/conditional_get"
-  autoload :Config, "rack/config"
-  autoload :ContentLength, "rack/content_length"
-  autoload :ContentType, "rack/content_type"
-  autoload :Deflater, "rack/deflater"
-  autoload :Directory, "rack/directory"
-  autoload :ETag, "rack/etag"
-  autoload :Events, "rack/events"
-  autoload :Files, "rack/files"
-  autoload :ForwardRequest, "rack/recursive"
-  autoload :Head, "rack/head"
-  autoload :Headers, "rack/headers"
-  autoload :Lint, "rack/lint"
-  autoload :Lock, "rack/lock"
-  autoload :MediaType, "rack/media_type"
-  autoload :MethodOverride, "rack/method_override"
-  autoload :Mime, "rack/mime"
-  autoload :MockRequest, "rack/mock_request"
-  autoload :MockResponse, "rack/mock_response"
-  autoload :Multipart, "rack/multipart"
-  autoload :NullLogger, "rack/null_logger"
-  autoload :QueryParser, "rack/query_parser"
-  autoload :Recursive, "rack/recursive"
-  autoload :Reloader, "rack/reloader"
-  autoload :Request, "rack/request"
-  autoload :Response, "rack/response"
-  autoload :RewindableInput, "rack/rewindable_input"
-  autoload :Runtime, "rack/runtime"
-  autoload :Sendfile, "rack/sendfile"
-  autoload :ShowExceptions, "rack/show_exceptions"
-  autoload :ShowStatus, "rack/show_status"
-  autoload :Static, "rack/static"
-  autoload :TempfileReaper, "rack/tempfile_reaper"
-  autoload :URLMap, "rack/urlmap"
-  autoload :Utils, "rack/utils"
-
-  module Auth
-    autoload :Basic, "rack/auth/basic"
-    autoload :AbstractHandler, "rack/auth/abstract/handler"
-    autoload :AbstractRequest, "rack/auth/abstract/request"
+  # Return the Rack release as a dotted string.
+  def self.release
+    VERSION
   end
 end
---
+++ lib/rack/autoloads.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module Rack
+  AUTOLOAD_CONSTANTS = {
+    BadRequest: 'rack/bad_request',
+    BodyProxy: 'rack/body_proxy',
+    Builder: 'rack/builder',
+    Cascade: 'rack/cascade',
+    CommonLogger: 'rack/common_logger',
+    ConditionalGet: 'rack/conditional_get',
+    Config: 'rack/config',
+    ContentLength: 'rack/content_length',
+    ContentType: 'rack/content_type',
+    Deflater: 'rack/deflater',
+    Directory: 'rack/directory',
+    ETag: 'rack/etag',
+    Events: 'rack/events',
+    Files: 'rack/files',
+    ForwardRequest: 'rack/recursive',
+    Head: 'rack/head',
+    Headers: 'rack/headers',
+    Lint: 'rack/lint',
+    Lock: 'rack/lock',
+    MediaType: 'rack/media_type',
+    MethodOverride: 'rack/method_override',
+    Mime: 'rack/mime',
+    MockRequest: 'rack/mock_request',
+    MockResponse: 'rack/mock_response',
+    Multipart: 'rack/multipart',
+    NullLogger: 'rack/null_logger',
+    QueryParser: 'rack/query_parser',
+    Recursive: 'rack/recursive',
+    Reloader: 'rack/reloader',
+    Request: 'rack/request',
+    Response: 'rack/response',
+    RewindableInput: 'rack/rewindable_input',
+    Runtime: 'rack/runtime',
+    Sendfile: 'rack/sendfile',
+    ShowExceptions: 'rack/show_exceptions',
+    ShowStatus: 'rack/show_status',
+    Static: 'rack/static',
+    TempfileReaper: 'rack/tempfile_reaper',
+    URLMap: 'rack/urlmap',
+    Utils: 'rack/utils',
+  }.freeze
+
+  AUTOLOAD_CONSTANTS.each do |const, path|
+    autoload const, path
+  end
+
+  module Auth
+    AUTH_AUTOLOAD_CONSTANTS = {
+      Basic: 'rack/auth/basic',
+      AbstractHandler: 'rack/auth/abstract/handler',
+      AbstractRequest: 'rack/auth/abstract/request',
+    }.freeze
+
+    AUTH_AUTOLOAD_CONSTANTS.each do |const, path|
+      autoload const, path
+    end
+  end
+end
